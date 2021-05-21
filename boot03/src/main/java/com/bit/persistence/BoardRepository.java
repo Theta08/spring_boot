f9639c5843bfddca1d6e2be293bd06d417ecfaca
pmw <pmwo9730@naver.com>
2021-05-21 15:58:46 +0900

    spring_test

--- boot03/src/main/java/com/bit/persistence/BoardRepository.java
+++ boot03/src/main/java/com/bit/persistence/BoardRepository.java
@@ -1,14 +1,44 @@
 package com.bit.persistence;
 
+
+import java.util.Collection;
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import com.bit.domain.Board;
 
 public interface BoardRepository extends CrudRepository<Board, Long>{
 
+	//findBy~
 	//title 조건으로 검색
-	public List<Board> findBoardByTitle(String title);
+	//where title=?
+	public List<Board> findByTitle(String title);
 	
+	//writer 조건으로 검색
+	//where write=?
+	public Collection<Board> findByWriter(String writer);
+	
+	//writer의 like 구문 처리
+	public List<Board> findByWriterContaining(String writer);
+	
+	//title에 '7'이 들어가 있는 레코드 검색
+	public List<Board> findByTitleContaining(String title);
+	//public List<Board> findByTitleLike(String title);
+	
+	//content에 '9채우기'로 끝나는 레코드 검색
+	public List<Board> findByContentEndingWith(String content);
+	
+	//or조건의 처리 ? title like '%title%' or content like '%content%' ???
+	public List<Board> findByTitleContainingOrContentContaining(String title,String content);
+	
+	//title like '%title%' and bno>
+	public List<Board> findByTitleContainingAndBnoGreaterThan(String title,Long bno);
+	
+	//bno> ? ORDER BY bno desc
+	public List<Board> findByBnoGreaterThanOrderByBnoDesc(Long bno);
+	
+	//bno>?order boy bno desc limit ?,?
+	public List<Board> findByBnoGreaterThanOrderByBnoDesc(Long bno,Pageable paging);
 }
--- boot03/src/test/java/com/bit/Boot03ApplicationTests.java
+++ boot03/src/test/java/com/bit/Boot03ApplicationTests.java
@@ -1,9 +1,14 @@
 package com.bit;
 
+import java.util.Collection;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.bit.domain.Board;
@@ -27,7 +32,7 @@ class Boot03ApplicationTests {
 		for (int i = 0; i < 200; i++) {
 			Board board=new Board();
 			board.setTitle("제목.."+i);
-			board.setContent("내용.."+i+" 체우기");
+			board.setContent("내용.."+i+"체우기");
 			board.setWriter("user0"+(i%10));
 			
 			repo.save(board);
@@ -36,8 +41,56 @@ class Boot03ApplicationTests {
 	
 	@Test
 	public void testByTitle() {
-		repo.findBoardByTitle("제목..155").forEach(board -> System.out.println(board));
-		
+		repo.findByTitle("제목..155").forEach(board -> System.out.println(board));	
+	}
+	
+	@Test
+	public void testByWriter() {
+		Collection<Board> results=repo.findByWriter("user00");
+		results.forEach(board -> System.out.println(board));
+	}
+	
+	@Test
+	public void testByWriterContaining() {
+		List<Board> result =repo.findByWriterContaining("05");
+		result.forEach(board -> System.out.println(board));
+	}
+	
+	@Test
+	public void testByTitleContaining() {
+		List<Board> result=repo.findByTitleContaining("7");
+		result.forEach(board -> System.out.println(board));
+	}
+	
+	@Test
+	public void testByContentEndingWith() {
+		List<Board> result=repo.findByContentEndingWith("9체우기");
+		result.forEach(board-> System.out.println(board));
+	}
+	
+	@Test
+	public void testTitleOrContain() {
+		List<Board> result=repo.findByTitleContainingOrContentContaining("77", "55");
+		result.forEach(board-> System.out.println(board));
+	}
+	
+	@Test
+	public void testTitleAndBno() {
+		List<Board> result=repo.findByTitleContainingAndBnoGreaterThan("5", 90L);
+		result.forEach(board-> System.out.println(board));
+	}
+	
+	@Test
+	public void testBnoOrderBy() {
+		List<Board> result=repo.findByBnoGreaterThanOrderByBnoDesc(90L);
+		result.forEach(board-> System.out.println(board));
+	}
+	
+	@Test
+	public void OrderByPaging() {
+		Pageable paing=PageRequest.of(0, 10);
+		List<Board> result=repo.findByBnoGreaterThanOrderByBnoDesc(0L,paing);
+		result.forEach(board-> System.out.println(board));
 	}
 
 }
