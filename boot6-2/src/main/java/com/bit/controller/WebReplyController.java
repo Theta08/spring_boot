60d9a69cccbc06039158b5bafb46c9b922e4219e
pmw <pmwo9730@naver.com>
2021-05-31 16:22:26 +0900

    0531

--- boot6-2/src/main/java/com/bit/controller/WebReplyController.java
+++ boot6-2/src/main/java/com/bit/controller/WebReplyController.java
@@ -5,8 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,6 +44,7 @@ public class WebReplyController {
 	 */
 	
 	//댓글 추가
+	@Transactional
 	@PostMapping("/{bno}")	//게시물 번호
 	public ResponseEntity<List<WebReply>> addReply(@PathVariable("bno")Long bno,
 			//@RequestBody(전송타입 json ): JSON 형태 -> 객체 타입 변환
@@ -61,5 +66,59 @@ public class WebReplyController {
 	private List<WebReply> getListByBoard(WebBoard board) throws RuntimeException{
 		log.info("getListByBoard.."+board);
 		return repo.getRepliesOfBoard(board);
+	}
+	
+	//뎃글삭제
+	// http://localhost/replies/300/1       Delete  믿에 200
+	@Transactional
+	@DeleteMapping("/{bno}/{rno}")
+	public ResponseEntity<List<WebReply>> remove(
+			@PathVariable("bno")Long bno,
+			@PathVariable("rno")Long rno
+			){
+		log.info("delete reply:"+rno);
+		repo.deleteById(rno);
+		WebBoard board=new WebBoard();
+		board.setBno(bno);
+		
+		return new ResponseEntity<>(getListByBoard(board),HttpStatus.OK);
+	} 
+	
+	//댓글 수정 modify
+	/*
+	 * http://localhost/replies/300			put
+	 * {
+		"rno":"2",
+		"replyText":"수정테스트",
+		"replyer":"홍길자"
+		}
+	 * 
+	 * */
+	@Transactional
+	@PutMapping("/{bno}")
+	public ResponseEntity<List<WebReply>> modify(
+			@PathVariable("bno")Long bno,
+			@RequestBody WebReply reply
+			){
+		log.info("modify reply:"+reply);
+		//조건에 만족하는 댓글 레코드
+		repo.findById(reply.getRno()).ifPresent(orgin->{
+			orgin.setReplyText(reply.getReplyText());
+			orgin.setReplyer(reply.getReplyer());
+			repo.save(orgin);
+		});
+		WebBoard board=new WebBoard();
+		board.setBno(bno);
+		return new ResponseEntity<>(getListByBoard(board),HttpStatus.OK);
+	}
+	
+	@GetMapping("/{bno}")
+	public ResponseEntity<List<WebReply>> getReplies(
+			@PathVariable("bno")Long bno){
+		log.info("get all Replies...........");
+		WebBoard board=new WebBoard();
+		board.setBno(bno);
+		
+		return new ResponseEntity<>(getListByBoard(board),HttpStatus.OK);
 	}
 }
